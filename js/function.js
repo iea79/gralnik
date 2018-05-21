cddc42a3517bcc3d046b4de23c83a3a2b6a11a11
Evgeny Ivanov <busforward@gmail.com>
2018-05-21 05:49:53 +0500

    Finish

--- js/function.js
+++ js/function.js
@@ -36,8 +36,8 @@ $(document).ready(function() {
 
 	// First screen full height
 	function setHeiHeight() {
-	    $('.full__height').css({
-	        minHeight: $(window).height() + 'px'
+	    $('.js-fullHeight').css({
+	        height: $(window).height() + 'px'
 	    });
 	}
 	setHeiHeight(); // устанавливаем высоту окна при первой загрузке страницы
@@ -50,28 +50,48 @@ $(document).ready(function() {
 	});
 
 	// Scroll to ID // Плавный скролл к элементу при нажатии на ссылку. В ссылке указываем ID элемента
-	// $('#main__menu a[href^="#"]').click( function(){ 
-	// 	var scroll_el = $(this).attr('href'); 
-	// 	if ($(scroll_el).length != 0) {
-	// 	$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
-	// 	}
-	// 	return false;
-	// });
+	$('[data-scroll-to]').click( function(){ 
+		var scroll_el = $(this).attr('href'); 
+		if ($(scroll_el).length != 0) {
+		$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+		}
+		return false;
+	});
 
-	// Stiky menu // Липкое меню. При прокрутке к элементу #header добавляется класс .stiky который и стилизуем
-    // $(document).ready(function(){
-    //     var HeaderTop = $('#header').offset().top;
-        
-    //     $(window).scroll(function(){
-    //             if( $(window).scrollTop() > HeaderTop ) {
-    //                     $('#header').addClass('stiky');
-    //             } else {
-    //                     $('#header').removeClass('stiky');
-    //             }
-    //     });
-    // });
-   	// setGridMatch($('[data-grid-match] .grid__item'));
-   	gridMatch();
+	$('.video__preview').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.video__thumbs'
+	});
+	$('.video__thumbs').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		asNavFor: '.video__preview',
+		arrows: false,
+		dots: true,
+		centerMode: true,
+		focusOnSelect: true,
+		responsive: [{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+			},
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		}]
+	});
+
+	checkOnResize();
+
+	formSubmit();
+
+
 });
 
 $(window).resize(function(event) {
@@ -80,31 +100,119 @@ $(window).resize(function(event) {
     if (TempApp.resized == windowWidth) { return; }
     TempApp.resized = windowWidth;
 
-	checkOnResize()
+	checkOnResize();
+
+
 });
 
 function checkOnResize() {
-   	// setGridMatch($('[data-grid-match] .grid__item'));
-   	gridMatch();
+	fontResize();
 }
 
-function gridMatch() {
-   	$('[data-grid-match] .grid__item').matchHeight({
-   		byRow: true,
-   	});
+function fontResize() {
+    var windowWidth = $(window).width();
+    if (windowWidth < 1440 && windowWidth >= 768) {
+    	var fontSize = windowWidth/15.05;
+    } else if (windowWidth < 768) {
+    	var fontSize = 68;
+    // } else if (windowWidth >= 1770) {
+    // 	var fontSize = 100;
+    }
+	$('body').css('fontSize', fontSize + '%');
 }
 
-// function setGridMatch(columns) {
-// 	var tallestcolumn = 0;
-// 	columns.removeAttr('style');
-// 	columns.each( function() {
-// 		currentHeight = $(this).height();
-// 		if(currentHeight > tallestcolumn) {
-// 			tallestcolumn = currentHeight;
-// 		}
-// 	});
-// 	setTimeout(function() {
-// 		columns.css('minHeight', tallestcolumn);
-// 	}, 100);
-// }
+// Видео для страницы how it works
+$(function () {
+    if ($(".js-youtube")) {
+        $(".js-youtube").each(function () {
+            // Зная идентификатор видео на YouTube, легко можно найти его миниатюру
+            $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + this.id + '/sddefault.jpg)');
 
+            // Добавляем иконку Play поверх миниатюры, чтобы было похоже на видеоплеер
+            $(this).append($('<div alt="Play" class="video__play"></div>'));
+
+        });
+
+        $('.video__play, .video__prev').on('click', function () {
+            // создаем iframe со включенной опцией autoplay
+            var videoId = $(this).closest('.youtube').attr('id');
+            var iframe_url = "https://www.youtube.com/embed/" + videoId + "?autoplay=1&autohide=1";
+            if ($(this).data('params')) iframe_url += '&' + $(this).data('params');
+
+            // Высота и ширина iframe должны быть такими же, как и у родительского блока
+            var iframe = $('<iframe/>', {
+                'frameborder': '0',
+                'src': iframe_url,
+                // 'width': $(this).width(),
+                // 'height': $(this).innerHeight()
+            })
+
+            // Заменяем миниатюру HTML5 плеером с YouTube
+            $(this).closest('.video__wrap').append(iframe);
+
+        });
+    }
+
+});
+
+
+function formSubmit() {
+    $("[type=submit]").on('click', function (e){ 
+        e.preventDefault();
+        var form = $(this).closest('.form');
+        var url = form.attr('action');
+        var form_data = form.serialize();
+        var field = form.find('[required]');
+        // console.log(form_data);
+
+        empty = 0;
+
+        field.each(function() {
+            if ($(this).val() == "") {
+                $(this).addClass('invalid');
+                // return false;
+                empty++;
+            } else {
+                $(this).removeClass('invalid');
+                $(this).addClass('valid');
+            }  
+        });
+
+        // console.log(empty);
+
+        if (empty > 0) {
+            return false;
+        } else {        
+            $.ajax({
+                url: url,
+                type: "POST",
+                dataType: "html",
+                data: form_data,
+                success: function (response) {
+                    // $('#success').modal('show');
+                    // console.log('success');
+                    // console.log(response);
+                    // console.log(data);
+                    document.location.href = "success.html";
+                },
+                error: function (response) {
+                    // $('#success').modal('show');
+                    // console.log('error');
+                    // console.log(response);
+                }
+            });
+        }
+
+    });
+
+    $('[name="policyConfirm"]').on('change', function(event) {
+        event.preventDefault();
+        var btn = $(this).closest('.form').find('.btn');
+        if ($(this).prop('checked')) {
+            btn.removeAttr('disabled');
+            // console.log('checked');
+        } else {
+            btn.attr('disabled', true);
+        }
+    });
+}
